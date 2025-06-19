8373b48de87f9be4b7f7fbc98e7c8b541b93a6f4
pisey1993 <pisey@peoplenpartners.com>
2025-06-19 13:30:27 +0700

    new file

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
+
+<?php
 include 'includes/header.php';
 require 'includes/db.php';
 
